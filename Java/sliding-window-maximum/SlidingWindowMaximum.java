997e11dd48d998a3f6ca64e7d27510d383528636
Bezus <ilbezus@icloud.com>
2026-07-14 17:47:01 +0300

    wip

--- Java/sliding-window-maximum/SlidingWindowMaximum.java
+++ Java/sliding-window-maximum/SlidingWindowMaximum.java
@@ -1,4 +1,6 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
 public class SlidingWindowMaximum {
 
@@ -15,10 +17,27 @@ public class SlidingWindowMaximum {
         return result;
     }
 
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+            deque.addLast(i);
+            if (i >= k - 1) {
+                result[idx++] = nums[deque.peekFirst()];
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
         int k = 3;
-        int[] result = maxSlidingWindowBrute(nums, k);
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(maxSlidingWindowBrute(nums, k)));
+        System.out.println(Arrays.toString(maxSlidingWindow(nums, k)));
     }
 }
