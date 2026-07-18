dfaf3bc2835651602fcefeb800b668485fe98482
Bezus <ilbezus@icloud.com>
2026-07-18 12:36:25 +0300

    wip dfs

--- JS/course-schedule/courseSchedule.js
+++ JS/course-schedule/courseSchedule.js
@@ -6,9 +6,28 @@ function canFinish(numCourses, prerequisites) {
     adj.get(a).push(b);
   }
 
-  console.log(adj);
+  const state = new Array(numCourses).fill(0);
+
+  function dfs(node) {
+    state[node] = 1;
+    for (const next of adj.get(node)) {
+      if (state[next] === 1) return false;
+      if (state[next] === 0) {
+        dfs(next);
+      }
+    }
+    state[node] = 2;
+    return true;
+  }
+
+  for (let i = 0; i < numCourses; i++) {
+    if (state[i] === 0) {
+      dfs(i);
+    }
+  }
 
   return true;
 }
 
 console.log(canFinish(2, [[1, 0]]));
+console.log(canFinish(2, [[1, 0], [0, 1]]));
