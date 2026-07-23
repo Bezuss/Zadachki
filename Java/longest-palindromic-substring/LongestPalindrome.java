5b6a5b298b8c7a8339cc1f0e2c50d2847032de08
Bezus <ilbezus@icloud.com>
2026-07-23 09:36:56 +0400

    fix indices

--- Java/longest-palindromic-substring/LongestPalindrome.java
+++ Java/longest-palindromic-substring/LongestPalindrome.java
@@ -2,26 +2,34 @@ public class LongestPalindrome {
 
     public static String longestPalindrome(String s) {
         if (s == null || s.length() == 0) return "";
-        String best = "";
+        int start = 0, end = 0;
         for (int i = 0; i < s.length(); i++) {
-            String odd = expand(s, i, i);
-            if (odd.length() > best.length()) best = odd;
-            String even = expand(s, i, i + 1);
-            if (even.length() > best.length()) best = even;
+            int[] odd = expand(s, i, i);
+            if (odd[1] - odd[0] > end - start) {
+                start = odd[0];
+                end = odd[1];
+            }
+            int[] even = expand(s, i, i + 1);
+            if (even[1] - even[0] > end - start) {
+                start = even[0];
+                end = even[1];
+            }
         }
-        return best;
+        return s.substring(start, end + 1);
     }
 
-    static String expand(String s, int lo, int hi) {
+    static int[] expand(String s, int lo, int hi) {
         while (lo >= 0 && hi < s.length() && s.charAt(lo) == s.charAt(hi)) {
             lo--;
             hi++;
         }
-        return s.substring(lo + 1, hi);
+        return new int[]{lo + 1, hi - 1};
     }
 
     public static void main(String[] args) {
         System.out.println(longestPalindrome("babad"));
         System.out.println(longestPalindrome("cbbd"));
+        System.out.println(longestPalindrome("a"));
+        System.out.println(longestPalindrome(""));
     }
 }
