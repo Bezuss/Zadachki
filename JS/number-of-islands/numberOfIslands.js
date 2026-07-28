cdb9cdaa6fb229bddd8969b679a40f9527d450fb
Bezus <ilbezus@icloud.com>
2026-07-28 10:03:34 +0400

    dfs works

--- JS/number-of-islands/numberOfIslands.js
+++ JS/number-of-islands/numberOfIslands.js
@@ -3,13 +3,27 @@ function numIslands(grid) {
 
   const rows = grid.length;
   const cols = grid[0].length;
+  const visited = Array.from({ length: rows }, () => new Array(cols).fill(false));
+
+  function dfs(r, c) {
+    if (r < 0 || c < 0 || r >= rows || c >= cols) return;
+    if (visited[r][c] || grid[r][c] === '0') return;
+
+    visited[r][c] = true;
+
+    dfs(r + 1, c);
+    dfs(r - 1, c);
+    dfs(r, c + 1);
+    dfs(r, c - 1);
+  }
 
   let count = 0;
 
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < cols; c++) {
-      if (grid[r][c] === '1') {
+      if (grid[r][c] === '1' && !visited[r][c]) {
         count++;
+        dfs(r, c);
       }
     }
   }
