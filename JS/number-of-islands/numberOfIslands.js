c8f1f181e7dda1b13bf7ba676ce36a700ea7bd1d
Bezus <ilbezus@icloud.com>
2026-07-28 21:02:33 +0400

    add bfs version

--- JS/number-of-islands/numberOfIslands.js
+++ JS/number-of-islands/numberOfIslands.js
@@ -31,10 +31,55 @@ function numIslands(grid) {
   return count;
 }
 
+function numIslandsBFS(grid) {
+  if (!grid || grid.length === 0) return 0;
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+  const visited = Array.from({ length: rows }, () => new Array(cols).fill(false));
+  let count = 0;
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (grid[r][c] === '1' && !visited[r][c]) {
+        count++;
+        const queue = [[r, c]];
+        visited[r][c] = true;
+
+        while (queue.length) {
+          const [cr, cc] = queue.shift();
+          const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+          for (const [dr, dc] of dirs) {
+            const nr = cr + dr;
+            const nc = cc + dc;
+            if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && !visited[nr][nc] && grid[nr][nc] === '1') {
+              visited[nr][nc] = true;
+              queue.push([nr, nc]);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return count;
+}
+
 const test1 = [
   ['1', '1', '0'],
   ['0', '1', '0'],
   ['0', '0', '1'],
 ];
 
+const test2 = [
+  ['1', '1', '1', '1', '0'],
+  ['1', '1', '0', '1', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '0', '0', '0'],
+];
+
 console.log(numIslands(test1));
+console.log(numIslandsBFS(test1));
+console.log(numIslands(test2));
+console.log(numIslandsBFS(test2));
