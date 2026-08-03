0da49ea7e95da658ccea07462bbbf79e999b0280
Bezus <ilbezus@icloud.com>
2026-08-03 08:14:16 +0300

    core logic

--- JS/merge-intervals/merge_intervals.js
+++ JS/merge-intervals/merge_intervals.js
@@ -3,9 +3,21 @@ function mergeIntervals(intervals) {
 
   const sorted = intervals.slice().sort((a, b) => a[0] - b[0]);
 
-  const result = [];
+  const result = [sorted[0]];
+
+  for (let i = 1; i < sorted.length; i++) {
+    const curr = sorted[i];
+    const last = result[result.length - 1];
+
+    if (curr[0] <= last[1]) {
+      last[1] = Math.max(last[1], curr[1]);
+    } else {
+      result.push(curr);
+    }
+  }
 
   return result;
 }
 
 console.log(mergeIntervals([[1,3],[2,6],[8,10],[15,18]]));
+console.log(mergeIntervals([[1,4],[4,5]]));
