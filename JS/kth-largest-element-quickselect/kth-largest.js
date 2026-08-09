52337a2512fd9d5c4bc6dae3a91313c087deffd2
Bezus <ilbezus@icloud.com>
2026-08-09 14:37:16 +0300

    wip

--- JS/kth-largest-element-quickselect/kth-largest.js
+++ JS/kth-largest-element-quickselect/kth-largest.js
@@ -1,3 +1,22 @@
+function swap(arr, i, j) {
+  const tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+}
+
+function partition(arr, lo, hi) {
+  const pivot = arr[hi];
+  let i = lo;
+  for (let j = lo; j < hi; j++) {
+    if (arr[j] < pivot) {
+      swap(arr, i, j);
+      i++;
+    }
+  }
+  swap(arr, i, hi);
+  return i;
+}
+
 function kthLargest(nums, k) {
   const sorted = [...nums].sort((a, b) => a - b);
   return sorted[sorted.length - k];
