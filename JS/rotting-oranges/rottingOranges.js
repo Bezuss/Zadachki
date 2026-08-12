f05468f33ca97c7c6c0d07a986714adf366d94f9
Bezus <ilbezus@icloud.com>
2026-08-12 14:54:11 +0300

    bfs working

--- JS/rotting-oranges/rottingOranges.js
+++ JS/rotting-oranges/rottingOranges.js
@@ -3,16 +3,42 @@ function orangesRotting(grid) {
   const cols = grid[0].length;
 
   let queue = [];
+  let fresh = 0;
 
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < cols; c++) {
       if (grid[r][c] === 2) {
         queue.push([r, c]);
+      } else if (grid[r][c] === 1) {
+        fresh++;
       }
     }
   }
 
-  return -1;
+  let minutes = 0;
+  const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+  while (queue.length > 0 && fresh > 0) {
+    const next = [];
+
+    for (const [r, c] of queue) {
+      for (const [dr, dc] of dirs) {
+        const nr = r + dr;
+        const nc = c + dc;
+        if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] === 1) {
+          grid[nr][nc] = 2;
+          fresh--;
+          next.push([nr, nc]);
+        }
+      }
+    }
+
+    queue = next;
+    minutes++;
+  }
+
+  if (fresh > 0) return -1;
+  return minutes;
 }
 
 const grid1 = [
