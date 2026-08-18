6d152c689c9499999610133b6bd1e8167d539705
Bezus <ilbezus@icloud.com>
2026-08-18 13:13:59 +0300

    linked list

--- JS/lru-cache/lru-cache.js
+++ JS/lru-cache/lru-cache.js
@@ -1,16 +1,49 @@
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
 class LRUCache {
   constructor(capacity) {
     this.capacity = capacity;
     this.map = new Map();
+    this.head = new Node(null, null);
+    this.tail = new Node(null, null);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+
+  remove(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+  }
+
+  addToFront(node) {
+    node.next = this.head.next;
+    node.prev = this.head;
+    this.head.next.prev = node;
+    this.head.next = node;
   }
 
   get(key) {
     if (!this.map.has(key)) return -1;
-    return this.map.get(key);
+    const node = this.map.get(key);
+    return node.value;
   }
 
   put(key, value) {
-    this.map.set(key, value);
+    if (this.map.has(key)) {
+      const node = this.map.get(key);
+      node.value = value;
+    } else {
+      const node = new Node(key, value);
+      this.map.set(key, node);
+      this.addToFront(node);
+    }
   }
 }
 
