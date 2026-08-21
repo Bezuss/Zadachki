3d41e90edf12bd26e066d53149e7b05e12fcd7ae
Bezus <ilbezus@icloud.com>
2026-08-21 22:22:39 +0300

    use map instead

--- JS/group-anagrams/group_anagrams.js
+++ JS/group-anagrams/group_anagrams.js
@@ -1,26 +1,15 @@
 function groupAnagrams(strs) {
-  const result = [];
-  const used = new Array(strs.length).fill(false);
+  const map = new Map();
 
-  for (let i = 0; i < strs.length; i++) {
-    if (used[i]) continue;
-    const group = [strs[i]];
-    used[i] = true;
-    for (let j = i + 1; j < strs.length; j++) {
-      if (used[j]) continue;
-      if (isAnagram(strs[i], strs[j])) {
-        group.push(strs[j]);
-        used[j] = true;
-      }
+  for (const s of strs) {
+    const key = s.split('').sort().join('');
+    if (!map.has(key)) {
+      map.set(key, []);
     }
-    result.push(group);
+    map.get(key).push(s);
   }
-  return result;
-}
 
-function isAnagram(a, b) {
-  if (a.length !== b.length) return false;
-  return a.split('').sort().join('') === b.split('').sort().join('');
+  return Array.from(map.values());
 }
 
 console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
