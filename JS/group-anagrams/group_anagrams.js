3a5ba90864daa523472c5799db2d82097335de33
Bezus <ilbezus@icloud.com>
2026-08-23 18:38:04 +0300

    edge cases

--- JS/group-anagrams/group_anagrams.js
+++ JS/group-anagrams/group_anagrams.js
@@ -15,11 +15,18 @@ function groupAnagrams(strs) {
 function getKey(s) {
   const counts = new Array(26).fill(0);
   for (const ch of s) {
-    counts[ch.charCodeAt(0) - 97]++;
+    const code = ch.charCodeAt(0) - 97;
+    if (code >= 0 && code < 26) counts[code]++;
   }
   return counts.join(',');
 }
 
-console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
-console.log(groupAnagrams(['']));
-console.log(groupAnagrams(['a']));
+function main() {
+  console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
+  console.log(groupAnagrams(['']));
+  console.log(groupAnagrams(['a']));
+  console.log(groupAnagrams([]));
+  console.log(groupAnagrams(['abc', 'cba', 'bca', 'xyz']));
+}
+
+main();
