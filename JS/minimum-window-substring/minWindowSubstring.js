3a6a4f11a738d7f2580ab847071a6ae9043b0bdf
Bezus <ilbezus@icloud.com>
2026-08-24 12:04:14 +0300

    sliding window

--- JS/minimum-window-substring/minWindowSubstring.js
+++ JS/minimum-window-substring/minWindowSubstring.js
@@ -1,26 +1,34 @@
 function minWindow(s, t) {
   if (!s || !t) return "";
 
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i; j < s.length; j++) {
-      const sub = s.slice(i, j + 1);
-      if (containsAll(sub, t)) {
-        return sub;
-      }
-    }
-  }
-  return "";
-}
-
-function containsAll(sub, t) {
   const need = {};
   for (const ch of t) {
     need[ch] = (need[ch] || 0) + 1;
   }
-  for (const ch of sub) {
-    if (need[ch]) need[ch]--;
+
+  let left = 0;
+  let missing = t.length;
+  let bestLen = Infinity;
+  let bestStart = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    const ch = s[right];
+    if (need[ch] > 0) missing--;
+    need[ch] = (need[ch] || 0) - 1;
+
+    if (missing === 0) {
+      while (need[s[left]] < 0) {
+        need[s[left]]++;
+        left++;
+      }
+      if (right - left + 1 < bestLen) {
+        bestLen = right - left + 1;
+        bestStart = left;
+      }
+    }
   }
-  return Object.values(need).every(v => v <= 0);
+
+  return bestLen === Infinity ? "" : s.slice(bestStart, bestStart + bestLen);
 }
 
 console.log(minWindow("ADOBECODEBANC", "ABC"));
