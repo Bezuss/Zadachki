a5800c780542ad471e09a542698174fcf1c9fec8
Bezus <ilbezus@icloud.com>
2026-09-03 14:01:47 +0300

    wip

--- JS/evaluate-reverse-polish-notation/rpn.js
+++ JS/evaluate-reverse-polish-notation/rpn.js
@@ -1,14 +1,24 @@
 function evalRPN(tokens) {
   const stack = [];
-  const ops = ['+', '-', '*', '/'];
+  const ops = new Set(['+', '-', '*', '/']);
 
   for (const tok of tokens) {
-    if (!ops.includes(tok)) {
+    if (ops.has(tok)) {
+      const b = stack.pop();
+      const a = stack.pop();
+      let res;
+      if (tok === '+') res = a + b;
+      else if (tok === '-') res = a - b;
+      else if (tok === '*') res = a * b;
+      else res = a / b;
+      stack.push(res);
+    } else {
       stack.push(Number(tok));
     }
   }
 
-  return stack[0];
+  return stack.pop();
 }
 
 console.log(evalRPN(['2', '1', '+', '3', '*']));
+console.log(evalRPN(['4', '13', '5', '/', '+']));
