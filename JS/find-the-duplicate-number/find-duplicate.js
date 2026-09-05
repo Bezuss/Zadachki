0208671f3daec5dfa3fd2330e8b3d69e5b217cf4
Bezus <ilbezus@icloud.com>
2026-09-05 09:41:19 +0300

    cycle detect

--- JS/find-the-duplicate-number/find-duplicate.js
+++ JS/find-the-duplicate-number/find-duplicate.js
@@ -1,10 +1,17 @@
 function findDuplicate(nums) {
-  const seen = new Set();
-  for (const n of nums) {
-    if (seen.has(n)) return n;
-    seen.add(n);
+  let slow = nums[0];
+  let fast = nums[0];
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow !== fast);
+
+  slow = nums[0];
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[fast];
   }
-  return -1;
+  return slow;
 }
 
 console.log(findDuplicate([1, 3, 4, 2, 2]));
