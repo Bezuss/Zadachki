256f8af2a0f636c3ac9a1afa72c24c9cfc275641
Bezus <ilbezus@icloud.com>
2026-09-05 12:52:41 +0300

    done

--- JS/find-the-duplicate-number/find-duplicate.js
+++ JS/find-the-duplicate-number/find-duplicate.js
@@ -1,4 +1,8 @@
 function findDuplicate(nums) {
+  if (!Array.isArray(nums) || nums.length < 2) {
+    throw new Error('invalid input');
+  }
+
   let slow = nums[0];
   let fast = nums[0];
   do {
@@ -14,5 +18,22 @@ function findDuplicate(nums) {
   return slow;
 }
 
-console.log(findDuplicate([1, 3, 4, 2, 2]));
-console.log(findDuplicate([3, 1, 3, 4, 2]));
+function findDuplicateBrute(nums) {
+  const seen = new Set();
+  for (const n of nums) {
+    if (seen.has(n)) return n;
+    seen.add(n);
+  }
+  return -1;
+}
+
+const tests = [
+  [1, 3, 4, 2, 2],
+  [3, 1, 3, 4, 2],
+  [1, 1],
+  [2, 5, 9, 6, 9, 3, 8, 9, 7, 1],
+];
+
+for (const t of tests) {
+  console.log(t.join(','), '->', findDuplicate(t), '(brute:', findDuplicateBrute(t) + ')');
+}
