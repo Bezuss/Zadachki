73b4494aee93a6dae7773a7c4ef29c4120688188
Bezus <ilbezus@icloud.com>
2026-09-09 19:36:22 +0300

    backtrack string

--- JS/longest-common-subsequence/lcs.js
+++ JS/longest-common-subsequence/lcs.js
@@ -19,5 +19,41 @@ function lcs(a, b) {
   return dp[m][n];
 }
 
+function lcsString(a, b) {
+  const m = a.length;
+  const n = b.length;
+  const dp = [];
+  for (let i = 0; i <= m; i++) {
+    dp.push(new Array(n + 1).fill(0));
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (a[i - 1] === b[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  let i = m, j = n;
+  let result = '';
+  while (i > 0 && j > 0) {
+    if (a[i - 1] === b[j - 1]) {
+      result = a[i - 1] + result;
+      i--;
+      j--;
+    } else if (dp[i - 1][j] >= dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
+    }
+  }
+
+  return result;
+}
+
 console.log(lcs('abcde', 'ace'));
 console.log(lcs('abc', 'abc'));
+console.log(lcsString('abcde', 'ace'));
