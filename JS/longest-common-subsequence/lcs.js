f78cdf3643cebeb54b357df93bb388df760ed8f2
Bezus <ilbezus@icloud.com>
2026-09-09 12:38:45 +0300

    wip dp

--- JS/longest-common-subsequence/lcs.js
+++ JS/longest-common-subsequence/lcs.js
@@ -1,5 +1,23 @@
 function lcs(a, b) {
-  // TODO
+  const m = a.length;
+  const n = b.length;
+  const dp = [];
+  for (let i = 0; i <= m; i++) {
+    dp.push(new Array(n + 1).fill(0));
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (a[i - 1] === b[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[m][n];
 }
 
 console.log(lcs('abcde', 'ace'));
+console.log(lcs('abc', 'abc'));
