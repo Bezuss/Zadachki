75a3defdf64a24c96fec8ed297885096a4eb43ce
Bezus <ilbezus@icloud.com>
2026-09-22 16:24:01 +0300

    dfs works

--- JS/course-schedule/courseSchedule.js
+++ JS/course-schedule/courseSchedule.js
@@ -6,7 +6,25 @@ function canFinish(numCourses, prerequisites) {
     graph.get(a).push(b);
   }
 
-  console.log(graph);
+  const state = new Array(numCourses).fill(0);
+
+  function dfs(node) {
+    if (state[node] === 1) return false;
+    if (state[node] === 2) return true;
+
+    state[node] = 1;
+    for (const next of graph.get(node)) {
+      if (!dfs(next)) return false;
+    }
+    state[node] = 2;
+    return true;
+  }
+
+  for (let i = 0; i < numCourses; i++) {
+    if (!dfs(i)) return false;
+  }
+  return true;
 }
 
-canFinish(2, [[1, 0]]);
+console.log(canFinish(2, [[1, 0]]));
+console.log(canFinish(2, [[1, 0], [0, 1]]));
