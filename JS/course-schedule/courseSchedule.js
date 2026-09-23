395469f5ed82a18741ba64a73879b4ff9fb97764
Bezus <ilbezus@icloud.com>
2026-09-23 09:44:15 +0300

    cleanup

--- JS/course-schedule/courseSchedule.js
+++ JS/course-schedule/courseSchedule.js
@@ -1,11 +1,5 @@
 function canFinish(numCourses, prerequisites) {
-  const graph = new Map();
-  for (let i = 0; i < numCourses; i++) graph.set(i, []);
-
-  for (const [a, b] of prerequisites) {
-    graph.get(a).push(b);
-  }
-
+  const graph = buildGraph(numCourses, prerequisites, false);
   const state = new Array(numCourses).fill(0);
 
   function dfs(node) {
@@ -13,7 +7,7 @@ function canFinish(numCourses, prerequisites) {
     if (state[node] === 2) return true;
 
     state[node] = 1;
-    for (const next of graph.get(node)) {
+    for (const next of graph[node]) {
       if (!dfs(next)) return false;
     }
     state[node] = 2;
@@ -27,12 +21,10 @@ function canFinish(numCourses, prerequisites) {
 }
 
 function findOrder(numCourses, prerequisites) {
-  const graph = new Map();
+  const graph = buildGraph(numCourses, prerequisites, true);
   const indegree = new Array(numCourses).fill(0);
-  for (let i = 0; i < numCourses; i++) graph.set(i, []);
 
-  for (const [a, b] of prerequisites) {
-    graph.get(b).push(a);
+  for (const [a] of prerequisites) {
     indegree[a]++;
   }
 
@@ -42,10 +34,11 @@ function findOrder(numCourses, prerequisites) {
   }
 
   const order = [];
-  while (queue.length) {
-    const node = queue.shift();
+  let head = 0;
+  while (head < queue.length) {
+    const node = queue[head++];
     order.push(node);
-    for (const next of graph.get(node)) {
+    for (const next of graph[node]) {
       indegree[next]--;
       if (indegree[next] === 0) queue.push(next);
     }
@@ -54,7 +47,22 @@ function findOrder(numCourses, prerequisites) {
   return order.length === numCourses ? order : [];
 }
 
-console.log(canFinish(2, [[1, 0]]));
-console.log(canFinish(2, [[1, 0], [0, 1]]));
-console.log(findOrder(4, [[1, 0], [2, 0], [3, 1], [3, 2]]));
-console.log(findOrder(2, [[1, 0], [0, 1]]));
+function buildGraph(numCourses, prerequisites, reversed) {
+  const graph = Array.from({ length: numCourses }, () => []);
+  for (const [a, b] of prerequisites) {
+    if (reversed) graph[b].push(a);
+    else graph[a].push(b);
+  }
+  return graph;
+}
+
+module.exports = { canFinish, findOrder };
+
+if (require.main === module) {
+  console.log(canFinish(2, [[1, 0]]));
+  console.log(canFinish(2, [[1, 0], [0, 1]]));
+  console.log(canFinish(5, []));
+  console.log(findOrder(4, [[1, 0], [2, 0], [3, 1], [3, 2]]));
+  console.log(findOrder(2, [[1, 0], [0, 1]]));
+  console.log(findOrder(1, []));
+}
