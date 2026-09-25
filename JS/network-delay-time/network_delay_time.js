9fd1f00ccb22abd4ecde2fcb0a86a5f8de9036e1
Bezus <ilbezus@icloud.com>
2026-09-25 20:54:01 +0300

    add heap

--- JS/network-delay-time/network_delay_time.js
+++ JS/network-delay-time/network_delay_time.js
@@ -1,3 +1,46 @@
+class MinHeap {
+  constructor() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  push(item) {
+    const items = this.items;
+    items.push(item);
+    let i = items.length - 1;
+    while (i > 0) {
+      const parent = (i - 1) >> 1;
+      if (items[parent][0] <= items[i][0]) break;
+      [items[parent], items[i]] = [items[i], items[parent]];
+      i = parent;
+    }
+  }
+
+  pop() {
+    const items = this.items;
+    const top = items[0];
+    const last = items.pop();
+    if (items.length) {
+      items[0] = last;
+      let i = 0;
+      while (true) {
+        let smallest = i;
+        const left = 2 * i + 1;
+        const right = 2 * i + 2;
+        if (left < items.length && items[left][0] < items[smallest][0]) smallest = left;
+        if (right < items.length && items[right][0] < items[smallest][0]) smallest = right;
+        if (smallest === i) break;
+        [items[smallest], items[i]] = [items[i], items[smallest]];
+        i = smallest;
+      }
+    }
+    return top;
+  }
+}
+
 function networkDelayTime(times, n, k) {
   const graph = new Map();
   for (const [u, v, w] of times) {
@@ -7,16 +50,16 @@ function networkDelayTime(times, n, k) {
 
   const dist = new Array(n + 1).fill(Infinity);
   dist[k] = 0;
-  const queue = [[0, k]];
+  const heap = new MinHeap();
+  heap.push([0, k]);
 
-  while (queue.length) {
-    queue.sort((a, b) => a[0] - b[0]);
-    const [d, u] = queue.shift();
+  while (heap.size()) {
+    const [d, u] = heap.pop();
     if (d > dist[u]) continue;
     for (const [v, w] of graph.get(u) || []) {
       if (d + w < dist[v]) {
         dist[v] = d + w;
-        queue.push([dist[v], v]);
+        heap.push([dist[v], v]);
       }
     }
   }
@@ -30,3 +73,4 @@ function networkDelayTime(times, n, k) {
 
 console.log(networkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2));
 console.log(networkDelayTime([[1, 2, 1]], 2, 1));
+console.log(networkDelayTime([[1, 2, 1]], 2, 2));
